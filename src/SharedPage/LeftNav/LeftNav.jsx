6aa7bc8d656f3e8f7a3a6e475ae41d3b63e0a846
JAHID HASAN <developerjahid@gmail.com>
2024-01-27 21:42:23 +0600

    leftNav added

--- src/SharedPage/LeftNav/LeftNav.jsx
+++ src/SharedPage/LeftNav/LeftNav.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [category, setCategory]= useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/category')
+        .then(res=> res.json())
+        .then(data=>setCategory(data))
+        .catch(error=>{
+            console.log(error)
+        })
+    },[])
+
     return (
         <div>
-            this is left
+            {
+                category.map(c=> <p key={c.id}>
+                    <Link>{c.category}</Link>
+                </p>)
+            }
         </div>
     );
 };
